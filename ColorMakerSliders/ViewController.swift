e09910dcd38a8f23596a51fefd6430f42bb36b9c
Michael Owens <mowens@ucdavis.edu>
2015-05-13 16:01:22 -0700

    added sliders to adjust rgb values added a view to display the color

--- ColorMakerSliders/ViewController.swift
+++ ColorMakerSliders/ViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliding(sender: AnyObject) {
+        let red: CGFloat = CGFloat(self.redSlider.value)
+        let green: CGFloat = CGFloat(self.greenSlider.value)
+        let blue: CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
-
 
 }
 
